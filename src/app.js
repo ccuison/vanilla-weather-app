bff57caf1c209641190d247a20cffe0469af00f4
ccuison <camelacuison@gmail.com>
2021-06-04 20:49:02 +0100

    Updated relevant icons from api

--- src/app.js
+++ src/app.js
@@ -38,10 +38,16 @@ function displayTemperature(response) {
   speedElement.innerHTML = windSpeed;
   let dateElement = document.querySelector("#date");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "399e957e8f76382378f1ff5b0308246a";
-let city = "Moscow";
+let city = "New York";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
