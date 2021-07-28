d09f7702d7f8767fbe2aa38c7787cf6c49641685
ccuison <camelacuison@gmail.com>
2021-07-28 09:03:34 +0100

    Integrated forecast API call

--- src/app.js
+++ src/app.js
@@ -21,6 +21,14 @@ function formatDate(timestamp) {
   return `${currentDay}, ${hours}:${minutes} `;
 }
 
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "399e957e8f76382378f1ff5b0308246a";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -46,6 +54,8 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   celsiusTemperature = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -91,7 +101,8 @@ let celsiusTemperature = null;
 
 search("New York");
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = ` <div class ="row">`;
@@ -120,5 +131,3 @@ function displayForecast() {
 
   forecastElement.innerHTML = forecastHTML;
 }
-
-displayForecast();
